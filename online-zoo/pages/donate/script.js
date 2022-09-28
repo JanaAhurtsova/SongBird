b6d68f81ef0232724cc615766c46157d3fcaf033
Jana Ahurtsova <byjankaaa@gmail.com>
2022-09-29 02:07:34 +0300

    feat: add adaptiv to tablet donate without footer

--- online-zoo/pages/donate/script.js
+++ online-zoo/pages/donate/script.js
@@ -20,3 +20,15 @@ dots.forEach(dot => {
         }
     });
 })
+
+//burger-menu
+
+const hamburger = document.querySelector('.hamburger');
+const hamburgerLine = document.querySelector('.hamburger__line');
+const burgerMenu = document.querySelector('.wrapper__header');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    hamburgerLine.classList.toggle('active');
+    burgerMenu.classList.toggle('active');
+})
