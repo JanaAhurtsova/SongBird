dfe70fae49d4da550691089e56baf03918d186e6
Jana Ahurtsova <byjankaaa@gmail.com>
2022-10-06 02:17:16 +0300

    feat: add carousel with pets

---
+++ online-zoo/pages/main/js/pets.js
@@ -0,0 +1,59 @@
+export const pets = [
+    {
+        id: 1,
+        urlToImg: '../../assets/images/main/animals/pandas.jpg',
+        name: 'GIANT PANDAS',
+        location: 'Native to Southwest China',
+        urlToIcon: '../../assets/icons/main/animals/banana-bamboo_icon.svg',
+    },
+    {
+        id: 2,
+        urlToImg: '../../assets/images/main/animals/eagle.jpg',
+        name: 'EAGLES',
+        location: 'Native to South America',
+        urlToIcon: '../../assets/icons/main/animals/meet-fish_icon.svg',
+    },
+    {
+        id: 3,
+        urlToImg: '../../assets/images/main/animals/gorilla.jpg',
+        name: 'GORILLAS',
+        location: 'Native to Congo',
+        urlToIcon: '../../assets/icons/main/animals/banana-bamboo_icon.svg',
+    },
+    {
+        id: 4,
+        urlToImg: '../../assets/images/main/animals/sloth.jpg',
+        name: 'TWO-TOED SLOTH',
+        location: 'Mesoamerica, South America',
+        urlToIcon: '../../assets/icons/main/animals/meet-fish_icon.svg',
+    },
+    {
+        id: 5,
+        urlToImg: '../../assets/images/main/animals/cheetah.jpg',
+        name: 'CHEETAHS',
+        location: 'Native to Africa',
+        urlToIcon: '../../assets/icons/main/animals/meet-fish_icon.svg',
+    },
+    {
+        id: 6,
+        urlToImg: '../../assets/images/main/animals/penguin.jpg',
+        name: 'PENGUINS',
+        location: 'Native to Antarctica',
+        urlToIcon: '../../assets/icons/main/animals/meet-fish_icon.svg',
+    },
+    {
+        id: 7,
+        urlToImg: '../../assets/images/main/animals/alligator.jpg',
+        name: 'ALLIGATORS',
+        location: 'Native to Southeastern U.S.',
+        urlToIcon: '../../assets/icons/main/animals/meet-fish_icon.svg',
+    },
+    {
+        id: 8,
+        urlToImg: '../../assets/images/main/animals/gorilla2.png',
+        name: 'GORILLAS',
+        location: 'Native to Congo',
+        urlToIcon: '../../assets/icons/main/animals/banana-bamboo_icon.svg',
+    },
+]
+
--- online-zoo/pages/main/js/script.js
+++ online-zoo/pages/main/js/script.js
@@ -1,24 +1,147 @@
-//burger-menu
+// //burger-menu
 
-const hamburger = document.querySelector('.hamburger');
-const hamburgerLine = document.querySelector('.hamburger__line');
-const burgerMenu = document.querySelector('.wrapper__header');
-const overlay = document.querySelector('.overlay');
+// const hamburger = document.querySelector('.hamburger');
+// const hamburgerLine = document.querySelector('.hamburger__line');
+// const burgerMenu = document.querySelector('.wrapper__header');
+// const overlayBurger = document.querySelector('.overlay__burger');
 
-hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('active');
-    hamburgerLine.classList.toggle('active');
-    burgerMenu.classList.toggle('active');
-    overlay.classList.toggle('active');
-})
+// hamburger.addEventListener('click', () => {
+//     hamburger.classList.toggle('active');
+//     hamburgerLine.classList.toggle('active');
+//     burgerMenu.classList.toggle('active');
+//     overlayBurger.classList.toggle('active');
+// })
 
-overlay.addEventListener('click', () => {
-    hamburger.classList.remove('active');
-    hamburgerLine.classList.remove('active');
-    burgerMenu.classList.remove('active');
-    overlay.classList.remove('active');
-})
+// overlayBurger.addEventListener('click', () => {
+//     hamburger.classList.remove('active');
+//     hamburgerLine.classList.remove('active');
+//     burgerMenu.classList.remove('active');
+//     overlayBurger.classList.remove('active');
+// })
 
 // carousel with pets 
-
+import { pets } from './pets.js';
+import { Cards} from './cards.js';
 const carousel = document.querySelector('.carousel');
+const itemLeft = document.querySelector('.item-left');
+const itemRight = document.querySelector('.item-right');
+const item = document.querySelector('.item');
+const prev = document.querySelector('.arrow__left');
+const next = document.querySelector('.arrow__right');
+let mqlBig = window.matchMedia('(max-width: 1600px)');
+let mqlSmall = window.matchMedia('(max-width: 1000px)');
+let mqlTablet = window.matchMedia('(max-width: 432px)');
+let mqlMobile = window.matchMedia('(max-width: 320px)');
+
+const generateCards = () => {
+    let cards = [];
+    pets.forEach(card => {
+        cards.push(new Cards(card))
+    });
+  return cards;
+}
+
+// window.onload = function() {
+//     // Render Cards
+//     if(pets) {
+//         renderCardsToDomLeft();
+//         renderCardsToDomRight();
+//     }
+// }
+
+const renderCardsToDomLeft = () => {
+    generateCards().sort(() => Math.random() - 0.5).splice(0, 6).forEach(card => {
+        itemLeft.append(card.generateCard());
+    })
+    console.log(itemLeft)
+}
+renderCardsToDomLeft();
+
+const renderCardsToDomRight = () => {
+    generateCards().sort(() => Math.random() - 0.5).splice(0, 6).forEach(card => {
+        itemRight.append(card.generateCard());
+    })
+    console.log(itemRight)
+}
+renderCardsToDomRight()
+
+const toLeft = () => {
+    carousel.classList.add('to_left');
+    prev.removeEventListener('click', toLeft);
+    next.removeEventListener('click', toRight);
+}
+
+const toRight = () => {
+    carousel.classList.add('to_right');
+    prev.removeEventListener('click', toLeft);
+    next.removeEventListener('click', toRight);
+}
+
+prev.addEventListener('click', toLeft);
+next.addEventListener('click', toRight);
+
+carousel.addEventListener('animationend', (animationEvent) => {
+    let changeItem;
+    if (animationEvent.animationName === 'left') {
+        changeItem = itemLeft;
+        carousel.classList.remove('to_left');
+        const leftItems = itemLeft.innerHTML;
+        item.innerHTML = leftItems;
+    } else {
+        changeItem = itemRight;
+        carousel.classList.remove('to_right');
+        const rightItems = itemRight.innerHTML;
+        item.innerHTML = rightItems;
+    }
+    
+    changeItem.innerHTML = '';
+    generateCards().sort(() => Math.random() - 0.5).splice(0, 6).forEach(card => {
+        changeItem.append(card.generateCard());
+    })
+
+    prev.addEventListener('click', toLeft);
+    next.addEventListener('click', toRight);
+})
+
+//Testimonials
+import { Article } from './articles.js';
+import { Modal } from './modal.js';
+import { data } from './feedbacks.js';
+const range = document.querySelector('.testimonial__scroll');
+
+window.onload = function() {
+
+    // Render Articles
+
+    if(data) {
+        renderArticlesToDom();
+    }
+}
+
+const renderArticlesToDom = () => {
+    let feedbackWrapper = getFeedbackWrapper();
+    generateArticles(data).forEach(article => {
+        feedbackWrapper.append(article.generateArticle());
+    })
+}
+
+const getFeedbackWrapper = () => {
+    const feedbackContainer = document.querySelector('.feedback-wrapper');
+    feedbackContainer.innerHTML = '';
+    return feedbackContainer;
+}
+
+const generateArticles = (data) => {
+    let articles = [];
+    data.forEach(article => {
+        articles.push(new Article(article))
+    });
+    return articles;
+}
+
+const feedbacks =generateArticles(data);
+let width = feedbacks[0].offsetWidth
+function feedbacksTransform() {
+
+}
+window.addEventListener("resize", e => (width = feedbacks[0].offsetWidth));
